dc59760d230ee4afa913b4fee4801ac8e86c79bc
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2025-07-08 22:11:02 +0000

    Added console tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./player.js"
 import "./domains.js"
+import {wizard} from "./archetypes.js"
 import {jasper} from "./player.js"
 import {wildMagicCard} from "./domains.js"
 
@@ -9,3 +10,7 @@ console.table(jasper)
 console.log("Jasper's Domains")
 
 console.table(jasper.domainEffects[0]) // table entire domain card's effects
+
+console.log("available wizard domains")
+console.log(wizard.domains)
+console.table(wizard)
