fcb7975443547f989cfc48e9735496c5e331ac0a
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2025-07-14 16:58:58 +0000

    Init weapon classes and associated functions

--- src/weapons.js
+++ src/weapons.js
@@ -1,6 +1,43 @@
-class MeleeWeapon {
-
+class Weapon {
+  constructor(trait,range,damage,bonus) {
+    this.trait = trait;
+    this.range = range;
+    this.damage = damage; // format of d6
+    this.bonus = bonus;  //  format of +2
+    this.features = [];
+  }
+  addFeature(name,desc) {
+    let newFeature = new WeaponFeature(name,desc);
+    this.features.push(newFeature);
+  }
+  adjustHopeCost(featureName,hopeCost) {
+    featureName.hopeCost = hopeCost;
+  }
+  adjustStressCost(featureName, stressCost) {
+    featureName.stressCost = stressCost;
+  }
 }
-class RangedWeapon {
-
+class WeaponFeature {
+  constructor(name,desc) {
+    this.name = name;
+    this.desc = desc;
+    this.hopeCost = 0;
+    this.stressCost = 0;
+  }
 }
+
+/*
+**** Weapon Model ****
+- trait
+- range
+- damage
+- damage bonus
+- multiple weapon abilities
+  ** Ability Entails **
+  - ability name
+  - ability description
+  - ability dmg
+  - ability buff
+  - hope cost
+  - stress cost
+*/
