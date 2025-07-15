50ffe0be17202a98ed44bb057a9960eacd22325a
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2025-07-15 22:11:49 +0000

    Removed redundant lines and added calls to create all communities

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./communities.js"
 import {wizard} from "./archetypes.js"
 import {jasper} from "./player.js"
 import {wildMagicCard} from "./domains.js"
-import { Community,highBorneDesc,highBorneBenefits,loreBorneDesc,loreBorneBenefits } from "./communities.js"
+import { Community } from "./communities.js"
+import {highBorneDesc,loreBorneDesc,orderBorneDesc,ridgeBorneDesc,seaBorneDesc,slyBorneDesc,underBorneDesc,wanderBorneDesc,wildBorneDesc} from "./data.js"
 import { createCard } from "./display.js"
 
 
@@ -12,6 +13,18 @@ const highborneCard = new Community("Highborne", highBorneDesc,highBorneBenefits
 createCard(highborneCard);
 /* const loreborneCard = new Community("Loreborne", loreBorneDesc,loreBorneBenefits);
 createCard(loreborneCard) */
+
+// Community Exports
+export const highborne = new Community("Highborne", highBorneDesc,highBorneBenefits);
+export const loreborne = new Community("Loreborne", loreBorneDesc,loreBorneBenefits);
+export const orderborne = new Community("Orderborne", orderBorneDesc,orderBorneBenefits);
+export const ridgeborne = new Community('Ridgeborne', ridgeBorneDesc, ridgeBorneBenefits);
+export const seaborne = new Community("Seaborne", seaBorneDesc, seaBorneBenefits);
+export const slyborne = new Community("Slyborne", slyBorneDesc, slyBorneBenefits);
+export const underborne = new Community("Underborne", underBorneDesc, underBorneBenefits);
+export const wanderborne = new Community("Wanderborne", wanderBorneDesc, wanderBorneBenefits);
+export const wildborne = new Community("Wildborne", wildBorneDesc, wildBorneBenefits);
+
 
 // tests
 jasper.domainEffects.push(wildMagicCard)
