7750fea4a8fde7bb4c980174c08d3531748d3eab
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2025-07-15 14:34:00 +0000

    Finished weapon and weaponFeature classes and related methods

--- src/weapons.js
+++ src/weapons.js
@@ -1,30 +1,46 @@
 class Weapon {
-  constructor(trait,range,damage,bonus) {
+  constructor(trait,range,damage,bonus,lore = null) {
     this.trait = trait;
     this.range = range;
     this.damage = damage; // format of d6
     this.bonus = bonus;  //  format of +2
     this.features = [];
+    this.lore = lore;
   }
-  addFeature(name,desc) {
+  addFeature(name,desc) { // init new object in features array
     let newFeature = new WeaponFeature(name,desc);
     this.features.push(newFeature);
   }
-  adjustHopeCost(featureName,hopeCost) {
-    featureName.hopeCost = hopeCost;
-  }
-  adjustStressCost(featureName, stressCost) {
-    featureName.stressCost = stressCost;
-  }
+
 }
 class WeaponFeature {
-  constructor(name,desc) {
+  constructor(name,desc,damage=null,buff=null) {
     this.name = name;
     this.desc = desc;
     this.hopeCost = 0;
     this.stressCost = 0;
+    this.damage = damage;
+    this.buff = buff;
+  }
+  adjustHopeCost(newHopeCost) { // increase or decrease player hope
+    this.hopeCost = newHopeCost;
+  }
+  adjustStressCost(newStressCost) { // increase or decrease player stress
+    this.stressCost = newStressCost;
   }
 }
+
+export default function createPlayerWeapon(player,) { // pass an array here or seperate inputs
+  let newWeapon = new Weapon(); // pass an array here or seperate inputs
+  player.equipmentArray.push(newWeapon);
+}
+
+/*
+**** STILL TO DO ****
+- finish createPlayerWeapon function
+- imports and exports
+*/
+
 
 /*
 **** Weapon Model ****
