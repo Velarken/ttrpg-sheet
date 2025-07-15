bb99e998a00675d12bd69a03997144871f2cf3b8
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2025-07-15 22:13:42 +0000

    Added imports

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
-import "./player.js"
-import "./domains.js"
-import "./communities.js"
 import {wizard} from "./archetypes.js"
 import {jasper} from "./player.js"
 import {wildMagicCard} from "./domains.js"
-import { Community } from "./communities.js"
+import createCommunity from "./communities.js"
 import {highBorneDesc,loreBorneDesc,orderBorneDesc,ridgeBorneDesc,seaBorneDesc,slyBorneDesc,underBorneDesc,wanderBorneDesc,wildBorneDesc} from "./data.js"
-import { createCard } from "./display.js"
 
 
-const highborneCard = new Community("Highborne", highBorneDesc,highBorneBenefits);
-createCard(highborneCard);
-/* const loreborneCard = new Community("Loreborne", loreBorneDesc,loreBorneBenefits);
-createCard(loreborneCard) */
-
-// Community Exports
-export const highborne = new Community("Highborne", highBorneDesc,highBorneBenefits);
-export const loreborne = new Community("Loreborne", loreBorneDesc,loreBorneBenefits);
-export const orderborne = new Community("Orderborne", orderBorneDesc,orderBorneBenefits);
-export const ridgeborne = new Community('Ridgeborne', ridgeBorneDesc, ridgeBorneBenefits);
-export const seaborne = new Community("Seaborne", seaBorneDesc, seaBorneBenefits);
-export const slyborne = new Community("Slyborne", slyBorneDesc, slyBorneBenefits);
-export const underborne = new Community("Underborne", underBorneDesc, underBorneBenefits);
-export const wanderborne = new Community("Wanderborne", wanderBorneDesc, wanderBorneBenefits);
-export const wildborne = new Community("Wildborne", wildBorneDesc, wildBorneBenefits);
+// Communities Creation
+const highborne = new Community("Highborne", highBorneDesc,highBorneBenefits);
+const loreborne = new Community("Loreborne", loreBorneDesc,loreBorneBenefits);
+const orderborne = new Community("Orderborne", orderBorneDesc,orderBorneBenefits);
+const ridgeborne = new Community('Ridgeborne', ridgeBorneDesc, ridgeBorneBenefits);
+const seaborne = new Community("Seaborne", seaBorneDesc, seaBorneBenefits);
+const slyborne = new Community("Slyborne", slyBorneDesc, slyBorneBenefits);
+const underborne = new Community("Underborne", underBorneDesc, underBorneBenefits);
+const wanderborne = new Community("Wanderborne", wanderBorneDesc, wanderBorneBenefits);
+const wildborne = new Community("Wildborne", wildBorneDesc, wildBorneBenefits);
 
 
 // tests
