a5e2979ae1749a3df9107dc12d4a094da71a9837
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2025-07-15 15:01:39 +0000

    Created base object framework for Ancestories and Features

--- src/ancestory.js
+++ src/ancestory.js
@@ -1,5 +1,24 @@
 class Ancestory {
-  constructor(){
-
+  constructor(lore,mainFeature,subFeature){
+    this.lore = lore;
+    this.mainFeature = mainFeature;
+    this.subFeature = subFeature;
   }
 }
+class AncestoryFeature {
+  constructor(name,desc,bonus=null,bonusFor=null) {
+    this.name = name;
+    this.desc = desc;
+    this.bonus = bonus;
+    this.bonusFor = bonusFor;
+  }
+}
+export default function addPlayerAncestory(player) {
+  let playerAncestory = new Ancestory(); // pass object or indiv calls
+  player.ancestory = playerAncestory;
+}
+/*
+**** TO DOS ****
+  - create function to handle custom ancestories
+  - finish addPLayerAncestory function
+*/
